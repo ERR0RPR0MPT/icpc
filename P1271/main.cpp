e7042b2dec65b7349b0cbe5f7a84c7a4c7c990d5
ERR0RPR0MPT <github@fwder.cn>
2024-10-29 20:01:36 +0800

    update

--- P1271/main.cpp
+++ P1271/main.cpp
@@ -1,6 +1,17 @@
+#include <algorithm>
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, m;
+    cin >> n >> m;
+    int arr[m + 5];
+    for (int i = 0; i < m; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + m);
+    for (int i = 0; i < m; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
